ccd8c8e8181e1ecdcd0faef92ba620244e6f45aa
lionet-pascualon <miltonpascualon543@gmail.com>
2026-04-05 00:05:28 -0300

    Create script.js
    esto es para "esconder" los productos

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("keyup", e => {
+  if (e.target.matches("#buscador")) {
+    document.querySelectorAll(".card").forEach(producto => {
+      producto.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+        ? producto.classList.remove("filtro")
+        : producto.classList.add("filtro");
+    });
+  }
+});
