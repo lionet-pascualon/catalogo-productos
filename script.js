488079feec83c16081d86f286f7447d2203e2bea
lionet-pascualon <miltonpascualon543@gmail.com>
2026-04-05 00:51:33 -0300

    Update script.js
    para el buscador encuentre mas facil los productos

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 document.addEventListener("keyup", e => {
   if (e.target.matches("#buscador")) {
+    const textoBusqueda = e.target.value.toLowerCase(); // Convertimos lo que escriben a minúsculas
+    
     document.querySelectorAll(".card").forEach(producto => {
-      producto.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+      producto.textContent.toLowerCase().includes(textoBusqueda)
         ? producto.classList.remove("filtro")
         : producto.classList.add("filtro");
     });
