20f0a3005a44ae86e7af8a1aeee25fde64168622
lionet-pascualon <miltonpascualon543@gmail.com>
2026-04-21 07:46:56 -0300

    Delete main.js
    borramos el main por que esta arraigado un problema

--- main.js
+++
@@ -1,8 +0,0 @@
-import { cargarProductos } from "/LeopardX/catalogo.js";
-import { iniciarEscuchaAuth, setupAuthForm } from "/LeopardX/auth.js";
-
-document.addEventListener('DOMContentLoaded', () => {
-  cargarProductos();
-  iniciarEscuchaAuth();
-  setupAuthForm();
-});
